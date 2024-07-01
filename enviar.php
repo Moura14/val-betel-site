5302c1bd5d7185f4e2fb0f823435b15af59c5a68
João Gabriel <jgmoura.dev@gmail.com>
2024-07-01 16:30:58 -0300

    first commit

---
+++ enviar.php
@@ -0,0 +1,24 @@
+<?php
+
+  $to = "jgmoura.dev@gmail.com";
+
+  $assunto = "Formulario"
+
+  $mensagem = $_POST['mensagem']." - ".$_POST['nome']
+
+  $email = $_POST['email']; //remetente
+
+
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+$headers[] = 'To: $to';
+$headers[] = 'From: $email';
+
+
+
+  $status = mail($to, $assunto, $mensagem, implode("\r\n", $headers) );
+  if($status == true){
+    print "Mensagem foi enviada com sucesso!";
+  }else{
+    print "Não foi possível enviar";
+  }
